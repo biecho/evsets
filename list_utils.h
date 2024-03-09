6a947ed6348395ba940a22b3905caa674d751d68
Biecho <dimeyer@student.ethz.ch>
2024-03-09 21:18:15 +0000

    List

--- list_utils.h
+++ list_utils.h
@@ -5,21 +5,13 @@
 #include "cache.h"
 
 int list_length(cache_block_t *ptr);
-cache_block_t *list_pop(cache_block_t **ptr);
-cache_block_t *list_shift(cache_block_t **ptr);
-void list_push(cache_block_t **ptr, cache_block_t *e);
-void list_append(cache_block_t **ptr, cache_block_t *e);
 void list_split(cache_block_t *ptr, cache_block_t **chunks, int n);
-cache_block_t *list_slice(cache_block_t **ptr, size_t s, size_t e);
-cache_block_t *list_get(cache_block_t **ptr, size_t n);
 void list_concat(cache_block_t **ptr, cache_block_t *chunk);
 void list_from_chunks(cache_block_t **ptr, cache_block_t **chunks, int avoid, int len);
-void list_set_id(cache_block_t *ptr, int id);
 void print_list(cache_block_t *ptr);
 
 void initialize_list(cache_block_t *ptr, unsigned long sz);
 void pick_n_random_from_list(cache_block_t *set, unsigned long stride, unsigned long set_size,
 			     unsigned long n);
-void generate_conflict_set(cache_block_t **ptr, cache_block_t **out, int rep, int threshold);
 
 #endif /* list_utils_H */
